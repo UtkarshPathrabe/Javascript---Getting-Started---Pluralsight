7f30c0c967e81e29c42a993c49acbfc7503799af
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-24 21:11:00 +0530

    Object Properties

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,2 +1,10 @@
-// showMessage('Hello');
-changePercentOff(32);
+let mySymbol = Symbol();
+let person = {
+	name: 'John',
+	age: 32,
+	partTime: false,
+	[mySymbol]: 'secrectInformation',
+};
+// person.age = 33;
+person['age'] = 44;
+showMessage(person.age);
