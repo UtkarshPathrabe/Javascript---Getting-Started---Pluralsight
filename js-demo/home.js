6f6b2b616e432c493071f56bddcedb7d8e70bcef
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-24 17:04:30 +0530

    Manipulating Strings

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,3 +1,8 @@
-let person = 'Andrea';
-let message = `Hello ${person}`;
-showMessage(message);
+// let message = 'Hello';
+// message = message.toLowerCase();
+// message = message.toUpperCase();
+// message = message.substring(1);
+// message = message.length;
+// showMessage(message);
+let message = '123';
+showMessage(message + 2);
