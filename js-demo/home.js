099d5cd6205168d4fcf349f04f1c09471ffb54da
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-24 21:02:29 +0530

    Using Functions

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,12 +1,2 @@
-let key = 42;
-function getSecretCode(value) {
-	let keyGenerator = function () {
-		let key = 12;
-		console.log('in keyGenerator: ', key);
-		return key;
-	};
-	let code = value * keyGenerator();
-	console.log('in getSecretCode: ', key);
-	return code;
-}
-showMessage(getSecretCode(2));
+// showMessage('Hello');
+changePercentOff(32);
