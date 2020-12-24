3881676ccebdc6cb1a129422126d452cb1cf3dab
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-24 17:09:32 +0530

    Boolean Variables

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,6 +1,3 @@
-// let amount = 123;
-// amount = amount.toString();
-// showMessage(typeof amount);
-
-let amount = Number.parseFloat('123.12');
-showMessage(amount);
+let saved = false;
+saved = !saved;
+showMessage(saved);
