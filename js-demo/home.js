4154f9b6eed40d400a394e399be83c2f0730380e
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-24 20:58:01 +0530

    Function Scope

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,5 +1,12 @@
+let key = 42;
 function getSecretCode(value) {
-	let code = value * 42;
+	let keyGenerator = function () {
+		let key = 12;
+		console.log('in keyGenerator: ', key);
+		return key;
+	};
+	let code = value * keyGenerator();
+	console.log('in getSecretCode: ', key);
 	return code;
 }
 showMessage(getSecretCode(2));
