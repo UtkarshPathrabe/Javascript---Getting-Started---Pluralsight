1d78bc4fe6e200005647d31bd8ffab13f22097d7
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-25 16:48:27 +0530

    slice and splice

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,5 +1,6 @@
-const values = ['a', 'b', 'c'];
-const newValues = values.slice(1, 2);
+const values = ['a', 'b', 'c', 'd', 'e'];
+const newValues = values.slice(1, 4);
 console.log(newValues);
+values.splice(2, 2);
 values.splice(1, 0, 'foo');
 console.log(values);
