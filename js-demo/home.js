a996326155c8898c0fe2c965368fc188c9d662b3
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-25 17:18:56 +0530

    Arrays in the DOM

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,18 +1,3 @@
-const values = ['a', 'b', 'ccc', 'dddd', 'e'];
-console.log(values.indexOf('c'), values.indexOf('v'));
-
-const newValues = values.filter(function (item) {
-	return item > 'b';
-});
-
-console.log(newValues);
-
-const found = values.find(function (item) {
-	return item.length > 1;
-});
-
-console.log(found);
-
-values.forEach(function (item) {
-	console.log(item);
-});
+const containers = document.getElementsByClassName('container');
+containers[2].classList.add('d-none');
+console.log(containers);
