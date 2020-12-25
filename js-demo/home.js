86c187fbc9c1bba8aa295a91d538a8716468184d
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-25 16:38:35 +0530

    Creating and Initializing Array

--- js-demo/home.js
+++ js-demo/home.js
@@ -1,11 +1,2 @@
-const button = document.getElementById('see-review');
-button.addEventListener('click', function () {
-	const review = document.getElementById('review');
-	if (review.classList.contains('d-none')) {
-		review.classList.remove('d-none');
-		button.textContent = 'CLOSE REVIEW';
-	} else {
-		review.classList.add('d-none');
-		button.textContent = 'SEE REVIEW';
-	}
-});
+const values = ['a', 'b', 'c'];
+console.log(values, typeof values, Array.isArray(values), values.length);
