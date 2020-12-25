856aff5165efcd100459fdf1d533ae5944f0cbaf
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-25 13:18:37 +0530

    Passing Objects to Functions

--- js-demo/home.js
+++ js-demo/home.js
@@ -2,9 +2,9 @@ let person = {
 	name: 'John',
 	age: 32,
 	partTime: false,
-	showInfo: function (realAge) {
-		showMessage(this.name + ' is ' + realAge);
-	},
 };
-
-person.showInfo(34);
+function incrementAge(person) {
+	person.age++;
+}
+incrementAge(person);
+showMessage(person.age);
